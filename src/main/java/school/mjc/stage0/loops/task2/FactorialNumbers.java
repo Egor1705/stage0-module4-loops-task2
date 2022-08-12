cb8171d36c343be3c6ab6f6379711ae960c1dffe
Egor1705 <BsuHelp@tut.by>
2022-08-12 18:49:43 +0300

    numbers

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,21 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int i = 0;
+//        while(i<=printToInclusive){
+//            if(i==0){
+//                i++;
+//                System.out.println(i);
+//            }
+//            i++;
+//            int j=1;
+//            while(j<=i){
+//                i=i*j;
+//                j++;
+//            }
+//
+//
+//        }
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,26 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int i = 0;
+
+        while (i <= printToInclusive) {
+            if (isPrime(i)==1){
+                System.out.println(i);
+            }
+            i++;
+            }
+    }
+    public static int isPrime(int num)
+    {
+        if(num<=1)
+        {
+            return 0;
+        }
+        for(int i=2;i<num/2;i++)
+        {
+            if((num%i)==0)
+                return 0;
+        }
+        return 1;
     }
 }
